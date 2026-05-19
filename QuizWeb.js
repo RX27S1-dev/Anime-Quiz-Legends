f1fb4b369dfb6c2d2cb4af7e7875b115e6726d9c
RX27S1-dev <saquibshakeel14@gmail.com>
2026-05-19 12:05:19 +0530

    Add files via upload

---
+++ QuizWeb.js
@@ -0,0 +1,133 @@
+let score = 0;
+
+document.getElementById("correct1").onclick = function () {
+
+    score++;
+
+    document.getElementById("score").innerText = "Score: " + score;
+
+    document.getElementById("correct1").disabled = true;
+
+    document.getElementById("correct1").style.backgroundColor = "green";
+
+}
+
+
+document.getElementById("correct2").onclick = function () {
+
+    score++;
+
+    document.getElementById("score").innerText = "Score: " + score;
+
+    document.getElementById("correct2").disabled = true;
+
+    document.getElementById("correct2").style.backgroundColor = "green";
+
+}
+
+
+document.getElementById("correct3").onclick = function () {
+
+    score++;
+
+    document.getElementById("score").innerText = "Score: " + score;
+
+    document.getElementById("correct3").disabled = true;
+
+    document.getElementById("correct3").style.backgroundColor = "green";
+
+}
+
+
+document.getElementById("correct4").onclick = function () {
+
+    score++;
+
+    document.getElementById("score").innerText = "Score: " + score;
+
+    document.getElementById("correct4").disabled = true;
+
+    document.getElementById("correct4").style.backgroundColor = "green";
+
+}
+
+
+document.getElementById("correct5").onclick = function () {
+
+    score++;
+
+    document.getElementById("score").innerText = "Score: " + score;
+
+    document.getElementById("correct5").disabled = true;
+
+    document.getElementById("correct5").style.backgroundColor = "green";
+
+}
+
+
+document.getElementById("correct6").onclick = function () {
+
+    score++;
+
+    document.getElementById("score").innerText = "Score: " + score;
+
+    document.getElementById("correct6").disabled = true;
+
+    document.getElementById("correct6").style.backgroundColor = "green";
+
+}
+
+document.getElementById("correct7").onclick = function () {
+
+    score++;
+
+    document.getElementById("score").innerText = "Score: " + score;
+
+    document.getElementById("correct7").disabled = true;
+
+    document.getElementById("correct7").style.backgroundColor = "green";
+
+}
+
+document.getElementById("correct8").onclick = function () {
+
+    score++;
+
+    document.getElementById("score").innerText = "Score: " + score;
+
+    document.getElementById("correct8").disabled = true;
+
+    document.getElementById("correct8").style.backgroundColor = "green";
+
+}
+
+document.getElementById("correct9").onclick = function () {
+
+    score++;
+
+    document.getElementById("score").innerText = "Score: " + score;
+
+    document.getElementById("correct9").disabled = true;
+
+    document.getElementById("correct9").style.backgroundColor = "green";
+
+}
+
+document.getElementById("correct10").onclick = function () {
+
+    score++;
+
+    document.getElementById("score").innerText = "Score: " + score;
+
+    document.getElementById("correct10").disabled = true;
+
+    document.getElementById("correct10").style.backgroundColor = "green";
+
+}
+
+document.getElementById("wrongn").onclick = function () {
+
+    document.getElementById("wrongn").style.backgroundColor = "red";
+
+}
+
